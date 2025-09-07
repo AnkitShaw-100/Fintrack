d696ff595f202a129710372abce8ed04422aa948
AnkitShaw-100 <ankitshaw6933@gmail.com>
2025-09-07 12:18:09 +0530

    feat: configure CORS to allow requests from the frontend domain

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://fintrack-fullstack.vercel.app", // your frontend domain
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/expenses", expenseRoutes);

ea3a058545a1e4972390ba003b5a86bf60c2ec91
AnkitShaw-100 <ankitshaw6933@gmail.com>
2026-01-01 19:42:04 +0530

    frontend url updated

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "https://finora-fullstack.vercel.app/",
+      "https://finora-fullstack.vercel.app",
       "http://localhost:5173",
     ],
     credentials: true,
